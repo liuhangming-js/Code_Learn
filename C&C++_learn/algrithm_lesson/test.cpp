85c8a985b39d4b9d3728f9757ff0babc22157cf8
刘航明 <liu_hangming@126.com>
2022-05-14 07:42:24 +0800

    5.14

--- C&C++_learn/algrithm_lesson/test.cpp
+++ C&C++_learn/algrithm_lesson/test.cpp
@@ -3,21 +3,11 @@
 #include<algorithm>
 using namespace std;
 
-class demo {
-    public:
-        int a;
-        int b;
-};
 
-bool com(demo x, demo y) {
-    if(x.a == y.a)
-        return x.b < y.b;
-    return x.a < y.a;
-}
 
 int main() {
-    vector<demo> arr = {{1, 2}, {5, 3}, {2, 4}, {3, 8}};
-    sort(arr.begin(), arr.end(), com);
-    for(int i = 0; i < arr.size(); i++) cout << arr[i].a << " " << arr[i].b << endl;
-    return 0;
+    vector<int> arr = {0, 4, 2, 6, 7};
+    int brr[] = {2, 4, 5, 1, 2};
+    cout << "MAX Value:" << *max_element(arr.begin(), arr.end()) << endl;
+    cout << "MAX Value:" << *max_element(brr, brr + (sizeof(brr) / sizeof(int))) << endl;
 }
