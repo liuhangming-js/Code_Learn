db2e90b99c60bc0360eebc3c5893c641f8ff7521
刘航明 <liu_hangming@126.com>
2022-05-19 00:04:51 +0800

    5.19

--- C&C++_learn/algrithm_lesson/test.cpp
+++ C&C++_learn/algrithm_lesson/test.cpp
@@ -3,11 +3,19 @@
 #include<algorithm>
 using namespace std;
 
-
-
 int main() {
-    vector<int> arr = {0, 4, 2, 6, 7};
-    int brr[] = {2, 4, 5, 1, 2};
-    cout << "MAX Value:" << *max_element(arr.begin(), arr.end()) << endl;
-    cout << "MAX Value:" << *max_element(brr, brr + (sizeof(brr) / sizeof(int))) << endl;
+    vector<int> arr = {10, 9, 2, 5, 3, 7, 101, 18};
+    int n = arr.size();
+    vector<int> dp(n, 0);
+
+    for(int i = 0; i < n; i++) {
+        dp[i] = 1;
+        for(int j = 0; j < i; j++) {
+            if(arr[j] < arr[i]) dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+
+    cout << *max_element(dp.begin(), dp.end());
+
+    return 0;
 }
